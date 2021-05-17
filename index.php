34019e8be93a542bb034f8264ef1b707c40fcecd
Gr4LyX <alexdu871@gmail.com>
2021-05-17 13:38:09 +0200

    first-commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<? 
+namespace Valarep;
+
+session_start();
---
+++ view/home.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require "head.html.php" ?>
+    <title>gestionnaire de courrier</title>
+</head>
+<body>
+    <h1>Bienvenue</h1>
+    
+</body>
+<?php require "scripts.html.php"?>
+</html>
